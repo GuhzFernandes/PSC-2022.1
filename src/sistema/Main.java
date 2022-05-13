4dd9b50b01292e759bfd6593aacc0cccc3d3f7ca
Gustavo Fernandes <gustavofernandes330@gmail.com>
2022-05-13 11:24:41 -0300

    Inicio do projeto

---
+++ src/sistema/Main.java
@@ -0,0 +1,12 @@
+package sistema;
+import sistema.Entidades.*;
+import sistema.GUI.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
