ad6019ea9fc4ed957f73e9541b4192422a57dc41
Gustavo Fernandes <gustavofernandes330@gmail.com>
2022-05-22 17:34:25 -0300

    Correção de bug na main + Atualização do nome do arquivo Main

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -2,7 +2,7 @@ package sistema;
 //import sistema.Entidades.*;
 import sistema.GUI.*;
 
-public class Main {
+public class Sistema {
 
     public static void main(String[] args) {
         Login login = new Login();
